e7c70ed572c61053632506694a3fb57a7cfd8c07
Ari Hatzimemos <hatzimemosari3@gmail.com>
2015-02-09 06:54:02 -0500

    hw 1

--- 01-Recursion/Recursion.java
+++ 01-Recursion/Recursion.java
@@ -28,4 +28,57 @@ public class Recursion{
 	    return countX(s.substring(1));
 	}
     }
+
+    public int bunnyEars2(int bunnies) {
+	if (bunnies == 0){
+	    return 0;
+	}
+	else if (bunnies%2==1){
+	    bunnies--;
+	    return 2 + bunnyEars2(bunnies);
+	}
+	else if (bunnies%2==0){
+	    bunnies--;
+	    return 3 + bunnyEars2(bunnies);
+	}
+    }
+    
+    public int strCount(String str, String sub) {
+	int i = 0;
+	if (str.length() == 0){
+	    i = i;
+	}
+	else if (str.substring(0,sub.length()).equals(sub)){
+	    i = 1 + strCount(str.substring(sub.length()), sub);
+	}
+	return i;
+    }
+
+    public int sumDigits(int n) {
+	int i = 0;
+	if (n == 0){
+	    i = i;
+	}
+	else {
+	    i = n%10 + sumDigits(n/10);
+	}
+	return i;
+    }
+
+    public String allStar(String str) {
+	String s = "";
+	if (str.length() == 0){
+	    s = str;
+	}
+	else if (str.length() == 1){
+	    s = str;
+	}
+	else if (str.length() == 2){
+	    s = str.substring(0,1) + "*" + str.substring(1);
+	}
+	else {
+	    s = str.substring(0,1) + "*" + allStar(str.substring(1));
+	}
+	return s;
+    }
 }
