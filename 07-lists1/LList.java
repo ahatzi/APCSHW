a3d6fe716eee38727a2d53c72b4041e601334ca8
Ari Hatzimemos <hatzimemosari3@gmail.com>
2015-03-18 00:22:52 -0400

    7 take 3

--- 07-lists1/LList.java
+++ 07-lists1/LList.java
@@ -20,18 +20,26 @@ public class LList {
     public String find(int n){
 	Node l2 = l;
 	for (int i=n;i>0;i--){
-	    Node l3 = l2.getNext;
+	    Node l3 = l2.getNext();
 	    l2 = l3;
 	}
 	return l2.getData();
     }
 
     public void insert(int n, String s){
-        Node l2 = l;
-	for (int i=n;i>0;i--){
-	    Node l3 = l2.getNext;
-	    l2 = l3;
+	Node a = new Node(s);
+        if (n == 0){
+	    l.add(a);
 	}
-	l2.setData(s);
+	else {
+	    Node l2 = l;
+	    for (int i=n;i>1;i--){
+		Node l3 = l2.getNext();
+		l2 = l3;
+	    }
+	    a.setNext(l2.getNext());
+	    l2.setNext(a);
+	    l = l2;
+	}
     }
 }
